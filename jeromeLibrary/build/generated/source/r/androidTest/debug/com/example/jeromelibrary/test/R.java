86450d1256618e427b93ded83c2172a3328ec2ee
Jerome <g0198602015@gmail.com>
2016-03-01 01:13:58 +0800

    1. show contract info when incoming telephone. 1. move some file

--- jeromeLibrary/build/generated/source/r/androidTest/debug/com/example/jeromelibrary/test/R.java
+++ jeromeLibrary/build/generated/source/r/androidTest/debug/com/example/jeromelibrary/test/R.java
@@ -15,10 +15,24 @@ public final class R {
         public static final int activity_vertical_margin=0x7f040001;
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int file_picker_back=0x7f020000;
+        public static final int file_picker_file=0x7f020001;
+        public static final int file_picker_folder=0x7f020002;
+        public static final int ic_launcher=0x7f020003;
+        public static final int incoming_icon=0x7f020004;
+    }
+    public static final class id {
+        public static final int exitButton=0x7f070001;
+        public static final int filePickerView=0x7f070000;
+        public static final int imageView=0x7f070002;
+        public static final int infoTextView=0x7f070005;
+        public static final int phoneNumberTextView=0x7f070004;
+        public static final int whoTextView=0x7f070003;
     }
     public static final class layout {
-        public static final int activity_main=0x7f030000;
+        public static final int activity_file_picker=0x7f030000;
+        public static final int activity_incoming_caller=0x7f030001;
+        public static final int activity_main=0x7f030002;
     }
     public static final class menu {
         public static final int main=0x7f060000;
